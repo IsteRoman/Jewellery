74328c8e7d1ab1ee9172d94cfea52ac0b18696d5
RomanIst <demonatr@mail.ru>
2021-11-08 23:53:21 +0500

    Внесены исправления после тестирования3

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,12 +1,22 @@
 import Swiper from 'swiper/bundle';
+const MAX_TABLET_WIDTH = 1023;
+const MIN_DESCTOP_WIDTH = 1024;
+const COST_NTH_NUMBER_NOT_DESCTOP = 2;
+const COST_NTH_NUMBER_DESCTOP = 4;
+
 
 const runSliderWork = () => {
+  if (!document.querySelector('.swiper')) {
+    return;
+  }
+  
   const swiper = new Swiper('.swiper', {
     spaceBetween: 30,
     direction: 'horizontal',
     slideClass: 'news__slider-item',
     slidesPerView: 2,
     slidesPerGroup: 2,
+    centeredSlides: false,
 
     pagination: {
       el: '.swiper-pagination',
@@ -45,6 +55,32 @@ const runSliderWork = () => {
       },
     },
   });
+
+  const useTabButton = () => {
+    const sliderBlock = document.querySelector('.news__slider')
+
+    const runTabFunction = (cost) => {
+      const skipElement = document.querySelectorAll(`.news__slider-item:nth-of-type(${cost}n)`)
+        skipElement.forEach((el) => {
+          el.classList.add('lastElement');
+        });
+        sliderBlock.addEventListener('keydown', (evt) => {
+          const eventTarger = evt.target;
+          const eventTargerParentElement = eventTarger.parentElement;
+
+          if (evt.keyCode === 9 && eventTargerParentElement.classList.contains('lastElement')) {
+            swiper.slideNext();
+          }
+        });
+      }
+
+    if (window.matchMedia(`(min-width:${MIN_DESCTOP_WIDTH}px)`).matches) {
+      runTabFunction(COST_NTH_NUMBER_DESCTOP);
+    } else if (window.matchMedia(`(max-width:${MAX_TABLET_WIDTH}px)`).matches) {
+      runTabFunction(COST_NTH_NUMBER_NOT_DESCTOP);
+    }
+  }
+  useTabButton();
 };
 
 export {runSliderWork};
